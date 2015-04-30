4e8a64887c36b604c43dfdf86307d1e8681daf7f
Björn <b@folbert.com>
2015-04-30 13:21:46 +0200

    Removed eu cookie stuff.
    Moved that to the vrick in fewbricks instead

--- functions.php
+++ functions.php
@@ -1,20 +1,3 @@
 <?php
 
-namespace fewture;
-
-/**
- * Functions that need to run on both frontend and backend
- */
-
-/**
- * Setting cookie to indicate that cookies are ok.
- * This can not be placed in the frontend functions.
- */
-function set_cookie() {
-
-  echo (setcookie('fewture_cookies_okd', '1', time()+(60*60*24*100), '/') ? '1' : '0');
-  die();
-
-}
-add_action('wp_ajax_fewture_cookies_okd', __NAMESPACE__ . '\\set_cookie');
-add_action('wp_ajax_nopriv_fewture_cookies_okd', __NAMESPACE__ . '\\set_cookie');
+namespace fewture\functions;
