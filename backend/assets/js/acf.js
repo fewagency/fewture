7eefaeb506a292ea303234395a733b419d882bfd
Björn <b@folbert.com>
2015-09-11 21:28:07 +0200

    Removed dev info function in favour of acf snitch

--- backend/assets/js/acf.js
+++ backend/assets/js/acf.js
@@ -1,12 +1,11 @@
 /**
- * Some stuff to improve the experience when using Adcanced Custom Fields
- * @todo Make dev info visible only by request by setting a cookie or something like that
+ * Some stuff to improve the experience when using Advanced Custom Fields
  */
 
 jQuery(document).ready(function() {
 
   /**
-   * Ask for confrimation before deleting module
+   * Ask for confirmation before deleting module
    */
   jQuery('body').on('click', '.acf-fc-remove', function(e) {
 
@@ -85,66 +84,6 @@ jQuery(document).ready(function() {
     $tableElements.css('display', 'none');
 
   });
-
-  /**
-   * Add developer info about the fields
-   */
-
-  function few_acf_add_dev_info() {
-
-    jQuery('.acf-field').each(function (index, elm) {
-
-      var info = '<div style="display: none; position: absolute; margin-top: -14px; z-index: 100; border: solid 1px #000; background: #fff; padding: 4px" class="acf-dev-info">'
-      info += '<b>Name:</b> ' + jQuery(elm).attr('data-name') + '<br>';
-      info += '<b>Key:</b> ' + jQuery(elm).attr('data-key');
-      info += '<div>';
-
-      var infoTrigger = ' <a href="#" class="acf-dev-info-trigger">?</a>';
-
-      var $acfLabelElm = jQuery('.acf-label:first', jQuery(elm));
-
-      $acfLabelElm.append(info);
-      jQuery('label', $acfLabelElm).append(infoTrigger);
-
-    });
-
-    var $devInfoElms = jQuery('.acf-dev-info');
-
-    $devInfoElms.on('mouseover', function (event, elm) {
-
-      jQuery(this).show();
-
-    });
-
-    $devInfoElms.on('mouseout', function (event, elm) {
-
-      jQuery(this).hide();
-
-    });
-
-    var $devInfoTriggerElms = jQuery('.acf-dev-info-trigger');
-
-    $devInfoTriggerElms.on('mouseover', function (event, elm) {
-
-      jQuery(this)
-        .parents('.acf-label:first')
-        .find('.acf-dev-info')
-        .show();
-
-    });
-
-    $devInfoTriggerElms.on('mouseout', function (event, elm) {
-
-      jQuery(this)
-        .parents('.acf-label:first')
-        .find('.acf-dev-info')
-        .hide();
-
-    });
-
-  }
-
-  //few_acf_add_dev_info();
 
 });
 
